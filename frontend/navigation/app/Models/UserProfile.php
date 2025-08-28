2d42496452573838867a80ef40376799e2d8d79f
TrongNghia <hoangtrongnghia2013@example.com>
2025-08-28 09:29:29 +0700

    user_profile table

---
+++ frontend/navigation/app/Models/UserProfile.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class UserProfile extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $table = 'user_profiles';
+    protected $fillable = [
+        'last_name',
+        'first_name',
+        'middle_name',
+        'dob',
+        'email',
+        'phone_number',
+        'country',
+        'gender',
+        'password',
+        'new_password',
+        'confirm_new_password',
+        'image',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+}
